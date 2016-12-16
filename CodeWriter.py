910e97d298ae55799a2c636a75cfd02d60b18485
Ofer <geon.arcan@gmail.com>
2016-12-16 19:02:02 +0200

    implementation without jumps, some minor fixes

--- CodeWriter.py
+++ CodeWriter.py
@@ -9,7 +9,7 @@ class CodeWriter:
         :param output: The output file/stream
         """
         self.output = output
-        self.output.write("@256\nD=A\n@SP\nM=D\n")
+        # self.output.write("@256\nD=A\n@SP\nM=D\n")
         self.file_name = ""
 
     def set_filename(self, file_name):
@@ -26,13 +26,13 @@ class CodeWriter:
         """
         commands = {'add': '@SP\nA=M-1\nD=M\n@SP\nM=M-1\nA=M-1\nD=D+M\nM=D\n'
                     ,'sub': '@SP\nA=M-1\nD=M\n@SP\nM=M-1\nA=M-1\nD=D-M\nM=D\n'
-                    ,'neg': '@SP\nA=M\nM=-M\n'
-                    ,'eq':  '@SP\nA=M-1\nD=M\n@SP\nM=M-1\nA=M-1\nD=D-M\n@EQ\nD;JEQ\n@SP\nA=M-1\nM=0\n@NEQ\n0;JMP\n(EQ)\n@SP\nA=M-1\nM=-1\n(NEQ)\n'
-                    ,'gt':  '@SP\nA=M-1\nD=M\n@SP\nM=M-1\nA=M-1\nD=M-D\n@GT\nD;JGT\n@SP\nA=M-1\nM=0\n@NGT\n0;JMP\n(GT)\n@SP\nA=M-1\nM=-1\n(NGT)\n'
-                    ,'lt': '@SP\nA=M-1\nD=M\n@SP\nM=M-1\nA=M-1\nD=M-D\n@LT\nD;JLT\n@SP\nA=M-1\nM=0\n@NLT\n0;JMP\n(LT)\n@SP\nA=M-1\nM=-1\n(NLT)\n'
+                    ,'neg': '@SP\nA=M-1\nM=-M\n'
+                    ,'eq':  '@SP\nA=M-1\nD=M\n@SP\nM=M-1\nA=M-1\nD=D-M\nA=D\nA=A>>\nD=D|A\nA=A>>\nD=D|A\nA=A>>\nD=D|A\nA=A>>\nD=D|A\nA=A>>\nD=D|A\nA=A>>\nD=D|A\nA=A>>\nD=D|A\nA=A>>\nD=D|A\nA=A>>\nD=D|A\nA=A>>\nD=D|A\nA=A>>\nD=D|A\nA=A>>\nD=D|A\nA=A>>\nD=D|A\nA=A>>\nD=D|A\nA=A>>\nD=D|A\nA=1\nD=D&A\n@SP\nA=M-1\nM=-D\n'
+                    ,'gt':  '@SP\nA=M-1\nD=M\n@SP\nM=M-1\nA=M-1\nD=M-D\nD=D>>\nD=D>>\nD=D>>\nD=D>>\nD=D>>\nD=D>>\nD=D>>\nD=D>>\nD=D>>\nD=D>>\nD=D>>\nD=D>>\nD=D>>\nD=D>>\nD=D>>\nM=D\n\n'
+                    ,'lt': '@SP\nA=M-1\nD=M\n@SP\nM=M-1\nA=M-1\nD=D-M\nD=D>>\nD=D>>\nD=D>>\nD=D>>\nD=D>>\nD=D>>\nD=D>>\nD=D>>\nD=D>>\nD=D>>\nD=D>>\nD=D>>\nD=D>>\nD=D>>\nD=D>>\nM=D\n\n'
                     , 'and': '@SP\nA=M-1\nD=M\n@SP\nM=M-1\nA=M-1\nD=D&M\nM=D\n'
                     ,'or': '@SP\nA=M-1\nD=M\n@SP\nM=M-1\nA=M-1\nD=D|M\nM=D\n'
-                    ,'not': '@SP\nA=M\nM=!M\n'}
+                    ,'not': '@SP\nA=M-1\nM=!M\n'}
         self.output.write(commands[command])
 
     def write_push_pop(self, command, segment, index):
@@ -44,26 +44,23 @@ class CodeWriter:
         :param index: int
         """
         segments = {'constant': '',
-                    'argument': '@ARG\nA=M+D\nD=M\n',
+                    'argument': '@ARG\nA=M+D\n',
                     'local': '@LCL\nA=M+D\n',
                     'this': '@THIS\nA=M+D\n',
                     'that': '@THAT\nA=M+D\n',
                     'pointer': '@3\nA=A+D\n',
                     'temp': '@5\nA=A+D\n'}
-        shortcuts = {'argument': 'ARGS', 'local': 'LCL', 'this': 'THIS\nA=M', 'that': 'THAT\nA=M', 'pointer': '3',
+        shortcuts = {'argument': 'ARG', 'local': 'LCL', 'this': 'THIS\nA=M', 'that': 'THAT\nA=M', 'pointer': '3',
                      'temp': '5'}
 
         #TODO: Figure out why commented form isn't working, get rid of double D=A
-        # set_value = 'D=M\n' if segment is not 'constant' else 'D=A\n'
-        if segment is 'constant':
-            set_value = 'D=M\n'
-        else:
-            set_value = "D=A\n"
+        # set_value = 'D=A\n' if segment is 'constant' else 'D=M\n'
+        set_value = '' if segment is 'constant' else 'D=M\n'
         set_index = "@{0}\nD=A\n"
 
         if command is C.C_PUSH:
             self.output.write(set_index.format(index)+segments[segment]+set_value+"@SP\nA=M\nM=D\n@SP\nM=M+1\n")
 
         elif command is C.C_POP:
-            self.output.write('@{0}\nD=A\n@'.format(index)+shortcuts[segment]
-                +'\nD=D+A\n@address\nM=D\n@SP\nM=M-1\nA=M\nD=M\n@address\nA=M\nM=D\n')
+            self.output.write((set_index+'@').format(index)+shortcuts[segment]
+                + '\nD=D+A\n@R15\nM=D\n@SP\nM=M-1\nA=M\nD=M\n@R15\nA=M\nM=D\n')
